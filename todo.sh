a2d8fd89a806a0f272a3e48077d1b6e309fc251a
DCFire <155055823+dontcatchfire@users.noreply.github.com>
2026-05-24 21:27:15 +0100

    Adds todo.sh file

---
+++ todo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Assigning tasks to respective variables 
+
+Task1="1. View all tasks"
+Task2="2. Add new task"
+Task3="3. Delete a task"
+Task4="4. Exit the program"
+
+# Prompting the user to choose a task
+
+
+
+
+
+
+
