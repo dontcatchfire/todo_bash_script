2a86799df9a36329006768c315c9f491972c68b4
DCFire <155055823+dontcatchfire@users.noreply.github.com>
2026-05-24 21:34:51 +0100

    Adds if statement for user selection

--- todo.sh
+++ todo.sh
@@ -10,9 +10,31 @@ Task4="4. Exit the program"
 
 # Prompting the user to choose a task
 
+echo "Hello, User. Here are your task options."
+
+echo "$Task1"
+echo "$Task2"
+echo "$Task3"
+echo "$Task4"
+
+echo -e "\n"
+
+echo "Choose a task [Hint - use the corresponding numbers]: "
+
+read task
 
 
-
+if [ $task == 1 ]; then
+	echo "$Task1 selected"
+elif [ $task == 2 ]; then 
+	echo "$Task2 selected"
+elif [ $task == 3 ]; then
+	echo "$Task3 selected"
+elif [ $task == 4 ]; then
+	echo "$Task4 selected"
+else 
+	echo "Please choose an available task."
+fi
 
 
 
