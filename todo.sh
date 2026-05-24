b27d541cab01b5b80729938821a95b3cf78bf899
DCFire <155055823+dontcatchfire@users.noreply.github.com>
2026-05-24 23:55:23 +0100

    Adds while loop and /home/dontcatchfire/todo.sh variable

--- todo.sh
+++ todo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+# While loop keeps the script running
+
+while true
+do
+
 # Assigning tasks to respective variables 
 
 Task1="1. View all tasks"
@@ -23,18 +28,24 @@ echo "Choose a task [Hint - use the corresponding numbers]: "
 
 read task
 
+# Assigning the file todo.txt to a variable
+
+ToDoTxt="$HOME/todo.txt"
+
 
 if [ $task == 1 ]; then
-	echo "$Task1 selected"
+	echo "$Task1 selected" && echo "$Task1" >> "ToDoTxt"
 elif [ $task == 2 ]; then 
-	echo "$Task2 selected"
+	echo "$Task2 selected" && echo "$Task2" >> "ToDoTxt"
 elif [ $task == 3 ]; then
-	echo "$Task3 selected"
+	echo "$Task3 selected" && echo "$Task3" >> "ToDoTxt"
 elif [ $task == 4 ]; then
-	echo "$Task4 selected"
+	echo "$Task4 selected" && echo "$Task4" >> "ToDoTxt"
 else 
 	echo "Please choose an available task."
 fi
 
+echo -e "\n"
 
+done
 
